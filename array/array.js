b59bf0ddbe4a85430e8b1b5d2498a5cc8a3abf92
Hoffm4ann <diego.a.hoffmann@gmail.com>
2024-01-23 11:07:15 -0300

    finalizando comentario

--- array/array.js
+++ array/array.js
@@ -2,5 +2,4 @@
 strutura de dados...uma forma de organizar os dados
 array ou vetor ou matriz mesma coisa
 para criar a array usa os [ e aqui dentro todos os dados separados por ,]
-contagem inicia no 0 ....  0, 1, 2, 3 zero participa da contagem....
-
+contagem inicia no 0 ....  0, 1, 2, 3 zero participa da contagem....*/
