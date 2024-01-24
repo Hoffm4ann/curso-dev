846656fa11f29ba94f04e1d9d4ed4414c4def593
Hoffm4ann <diego.a.hoffmann@gmail.com>
2024-01-24 11:06:25 -0300

    finalização introdução a js

---
+++ Function/function.js
@@ -0,0 +1,18 @@
+/*
+functions / funções
+trecho de codigo que eh executado
+somente quando solicitado */
+
+const variavel = "abacatinho"
+
+console.log(variavel)
+
+function nomeNaTela(){
+    console.log("Diego")
+    }
+/*função so vai executar quando for chamada,
+ por causa da funtion ela executou ate aqui
+ e agora vai executar a parte do cogigo abaixo 
+ por que esta sendo chamada
+ */
+    nomeNaTela()
---
+++ if&else/if&else.js
@@ -0,0 +1,23 @@
+/*
+Controlador de Fluxo
+IF(SE) = FAÇA ISSO
+ElSE(Se nao) = executa o ELSE
+
+Operadores de comparaçao
+> maior que
+< menor que
+== igual que
+*/
+ const notaDoAluno = 10
+ const notaDeCorte = 5
+ if(notaDoAluno > notaDeCorte){
+//O Que sera executado, se o if for verdadeiro
+console.log("Parabéns, vc passou de ano")
+ }
+ else{
+    //se o if for falso, ele entra aqui
+    console.log("Vc foi reprovado")
+ }
+ /* quando eu uso um = eu estou atribuindo uma variavel,
+ quando eu coloco dois == eu estou comparando */
+ 
