0eb97631857d898aed705d04ec0cb8a109c639d1
Hoffm4ann <diego.a.hoffmann@gmail.com>
2024-02-02 14:59:56 -0300

    organização pastas

--- JavaScript/Seletores Js/script.js
+++ JavaScript/Seletores Js/script.js
@@ -16,27 +16,27 @@ console.log(input)
 /*assim ele esta pegando o valor do placeholder,
  mais adiante pegara o valor digitado*/
 
-const elements = document.getElementsByClassName("paragraph-js")
+const element = document.getElementsByClassName("paragraph-js")
 
- console.log(elements)
+ console.log(element)
 
  /*button esta com duas classes ao mesmo tempo no html,
   no console do inspect apasrece o html collection*/
 
-  const elements = document.getElementsByTagName("p")
+  const element = document.getElementsByTagName("p")
 
- console.log(elements)
+ console.log(element)
  /* nesse caso ela vai achar todos os paragrafos, 
  mas poderia ser h1 ....etc*/
 
- const elements = document.getElementsByName("nome-completo")
+ const element = document.getElementsByName("nome-completo")
 
- console.log(elements)
+ console.log(element)
  /* assim ele vai e busca o nome que esta no input no html*/
 
- const elements = document.querySelector("#main-input")
+ const element = document.querySelector("#main-input")
 
- console.log(elements)
+ console.log(element)
  /*nesse caso mesmo com mais de um ele so vai achar o priomeiro
  # = id
  . = class
@@ -45,6 +45,14 @@ const elements = document.getElementsByClassName("paragraph-js")
  */
  const elements = document.querySelectorAll("p")
 
- console.log(elements)
+ console.log(element)
 /*nesse caso ele tras tudo o que vc colocar para procurar*/
+
+const element = document.querySelector("paragraph-js")
+element.innerHTML = "TEXTO NOVO <b>Olá</b>"
+
+console.log(element.textContent) //Só HTML
+console.log(element.innerText) //Leva em conta o css
+console.log(element.innerHTML) //tras tudo
+
 
